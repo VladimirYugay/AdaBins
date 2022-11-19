36ab922809f028f08c47b02c492e2c155ab53ac6
Vladimir Yugay <yugay@atcremers79.in.tum.de>
2022-11-19 18:21:25 +0100

    Update setup py

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 
 def find_version():
-    version_file = 'adabins/__init__.py'
-    with open(version_file, 'r') as f:
-        exec(compile(f.read(), version_file, 'exec'))
-    return locals()['__version__']
+    version_file = "adabins/__init__.py"
+    with open(version_file, "r") as f:
+        exec(compile(f.read(), version_file, "exec"))
+    return locals()["__version__"]
 
- 
 
 setup(
-    name='adabins',
+    name="adabins",
     version=find_version(),
-    description='A library for deep learning person re-ID in PyTorch',
-    author='Kaiyang Zhou',
-    license='MIT',
-    url='https://github.com/KaiyangZhou/deep-person-reid',
+    description="A library for deep learning person re-ID in PyTorch",
+    author="Vladimir Yugay",
+    license="MIT",
+    url="https://github.com/VladimirYugay/AdaBins",
     packages=find_packages(),
-    keywords=['Person Re-Identification', 'Deep Learning', 'Computer Vision'],
-    
+    keywords=["Deep Learning", "Computer Vision", "Depth Estimation"],
 )
